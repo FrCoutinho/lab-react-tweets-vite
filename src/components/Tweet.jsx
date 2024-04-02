0d590a40ca76f41009a080f87f726585fd0e5d8a
Francisco Coutinho <francisco.coutinho.so@gmail.com>
2024-04-02 01:42:19 +0100

    FT_PT_FranciscoCoutinho

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import ProfileImage from './ProfileImage';
+import User from './User';
+import Timestamp from './Timestamp';
+import Message from './Message';
+
+
 function Tweet() {
   return (
     <div className="tweet">
